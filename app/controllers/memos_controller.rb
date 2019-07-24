37f915e55dda164daeb364849b35ffd2276f351e
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-07-24 13:37:07 +0900

    メモ一覧画面の作成

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,2 +1,16 @@
 class MemosController < ApplicationController
+  before_action :set_memo, only: [:index]
+  before_action :set_folder, only: [:index]
+
+  def index
+  end
+
+  private
+  def set_memo
+    @memos = Memo.where(folder_id: params[:folder_id]).order("name ASC")
+  end
+
+  def set_folder
+    @folder_id = params.permit(:folder_id)
+  end
 end
