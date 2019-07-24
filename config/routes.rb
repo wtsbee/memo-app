b07c9db024ee951e418845207b512c04ed8eb7b6
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-07-24 09:25:34 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'memos#index'
-  resources :memos, only: [:index]
+  root 'folders#index'
+  resources :folders, only: [:index, :new, :create, :edit, :destroy] do
+    resources :memos, only: [:index, :show, :new, :create]
+  end
 end
