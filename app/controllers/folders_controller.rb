595c0f7f6331b8d00f9f49f8b06dc884f070a4bb
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-07-24 09:40:18 +0900

    フォルダ一覧画面の作成

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,2 +1,21 @@
 class FoldersController < ApplicationController
+  def index
+    @folders = Folder.all.includes(:memos).order("name ASC")
+  end
+
+  def create
+    @folder = Folder.new(folder_params)
+    if @folder.save
+      respond_to do |format|
+        format.html
+        format.json
+    else
+      render :index
+    end
+  end
+
+  private
+  def folder_params
+    params.permit(:name).merge(user_id: current_user.id)
+  end
 end
