74766313c38fdc60d2c81ba78f57c342a4a9f249
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-07-27 05:53:31 +0900

    コントローラーの編集

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,26 +1,64 @@
 class MemosController < ApplicationController
   before_action :set_memo, only: [:index]
+  before_action :set_edit_memo, only: [:edit, :update, :show]
   before_action :set_folder, only: [:index, :create]
 
   def index
+  end
+
+  def show
+    # binding.pry
+    # @memo = Memo.new(memo_params)
+    # if @memo.save
+    #   respond_to do |format|
+    #     format.html
+    #     format.json
+    #   end
+    # else
+    #   binding.pry
+    #   render :index
+    # end
   end
 
   def create
     @memo = Memo.new(memo_params)
     if @memo.save
       respond_to do |format|
-        format.html { redirect_to root_path, notice: 'メッセージが送信されました' }
+        format.html
         format.json
       end
     else
-      binding.pry
       render :index
     end
+  end
+
+  def edit
+    # binding.pry
+    gon.content = @memo.content
+  end
+  
+  def update
+    # binding.pry
+    @memo.update(memo_update_params)
+    # binding.pry
+    redirect_to folder_memo_path(id: @memo.id, folder_id: @memo.folder.id)
   end
 
   private
   def memo_params
     params.permit(:name).merge(folder_id: @folder_id)
+  end
+
+  def memo_update_params
+    params.permit(:content)
+  end
+
+  def memo_show_params
+    params.permit(:name, :context).merge(folder_id: @folder_id)
+  end
+
+  def set_edit_memo
+    @memo = Memo.find(params[:id])
   end
 
   def set_memo
