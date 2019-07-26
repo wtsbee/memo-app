8eeb1162e70dfadceae3b41ddd55e411027ff4c2
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-07-27 05:31:07 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'folders#index'
   resources :folders, only: [:index, :new, :create, :edit, :destroy] do
-    resources :memos, only: [:index, :show, :new, :create]
+    resources :memos, only: [:index, :show, :new, :create, :update, :edit]
   end
 end
