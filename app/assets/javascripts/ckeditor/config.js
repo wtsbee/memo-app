04626ea716a81d62dfcf68ec12635ffffe594ec5
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-08-03 10:37:04 +0900

    stylesSetの別ファイル化

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -80,23 +80,24 @@ CKEDITOR.editorConfig = function( config )
   CKEDITOR.config.enterMode = 2;
   config.font_names='メイリオ,Meiryo; "Yu Gothic Medium","游ゴシック Medium",YuGothic,"游ゴシック体",YuGothicM,"Yu Gothic"; ＭＳ Ｐゴシック; ＭＳ ゴシック; "游明朝","Yu Mincho",YuMincho; ＭＳ Ｐ明朝; ＭＳ 明朝; Arial/Arial, Helvetica, sans-serif; Comic Sans MS/Comic Sans MS, cursive; Courier New/Courier New, Courier, monospace; Georgia/Georgia, serif; Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif; Tahoma/Tahoma, Geneva, sans-serif; Times New Roman/Times New Roman, Times, serif; Trebuchet MS/Trebuchet MS, Helvetica, sans-serif; Verdana/Verdana, Geneva, sans-serif';
 
-  config.stylesSet = 'my_styles';
+  // config.stylesSet = 'my_styles';
+  config.stylesSet = 'my_styles:/styles.js'; 
 
-  CKEDITOR.stylesSet.add( 'my_styles', [
-    // Block Styles
-    { name: 'テキスト文',  element: 'p', styles: { 'font-size': '1em' } },
-    { name: '標準テキスト文',  element: 'p',  styles: { 'font-size': '1em','margin': '10px 0 10px 0' } },
-    { name: '段1-テキスト文',  element: 'p',  styles: { 'font-size': '1em','margin': '10px 0 10px 10px' } },
-    { name: '段2-テキスト文',  element: 'p',  styles: { 'font-size': '1em','margin': '10px 0 10px 25px' } },
-    { name: '段3-テキスト文',  element: 'p',  styles: { 'font-size': '1em','margin': '10px 0 10px 40px' } },
-    { name: '段4-テキスト文',  element: 'p',  styles: { 'font-size': '1em','margin': '10px 0 10px 55px' } },
-    // Inline Styles
-    { name: 'マーカー黄',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #ffff66 45%)' } },
-    { name: 'マーカーピンク',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #ffc9db 45%)'  } },
-    { name: 'マーカー水色',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #acffff 45%)'  } },
-    { name: 'マーカー緑',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #b8ff92 45%)'  } },
-    { name: 'マーカーベージュ',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #ffd791 45%)'  } },
-    { name: 'マーカーグレー',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #ddd 45%)'  } },
-    { name: 'ライン赤',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 90%, #ff5a5a 90%)'  } },
-  ] );
+  // CKEDITOR.stylesSet.add( 'my_styles', [
+  //   // Block Styles
+  //   { name: 'テキスト文',  element: 'p', styles: { 'font-size': '1em' } },
+  //   { name: '標準テキスト文',  element: 'p',  styles: { 'font-size': '1em','margin': '10px 0 10px 0' } },
+  //   { name: '段1-テキスト文',  element: 'p',  styles: { 'font-size': '1em','margin': '10px 0 10px 10px' } },
+  //   { name: '段2-テキスト文',  element: 'p',  styles: { 'font-size': '1em','margin': '10px 0 10px 25px' } },
+  //   { name: '段3-テキスト文',  element: 'p',  styles: { 'font-size': '1em','margin': '10px 0 10px 40px' } },
+  //   { name: '段4-テキスト文',  element: 'p',  styles: { 'font-size': '1em','margin': '10px 0 10px 55px' } },
+  //   // Inline Styles
+  //   { name: 'マーカー黄',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #ffff66 45%)' } },
+  //   { name: 'マーカーピンク',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #ffc9db 45%)'  } },
+  //   { name: 'マーカー水色',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #acffff 45%)'  } },
+  //   { name: 'マーカー緑',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #b8ff92 45%)'  } },
+  //   { name: 'マーカーベージュ',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #ffd791 45%)'  } },
+  //   { name: 'マーカーグレー',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #ddd 45%)'  } },
+  //   { name: 'ライン赤',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 90%, #ff5a5a 90%)'  } },
+  // ] );
 }
---
+++ app/assets/javascripts/styles.js
@@ -0,0 +1,17 @@
+CKEDITOR.stylesSet.add( 'my_styles', [
+  // Block Styles
+  { name: 'テキスト文',  element: 'p', styles: { 'font-size': '1em' } },
+  { name: '標準テキスト文',  element: 'p',  styles: { 'font-size': '1em','margin': '10px 0 10px 0' } },
+  { name: '段1-テキスト文',  element: 'p',  styles: { 'font-size': '1em','margin': '10px 0 10px 10px' } },
+  { name: '段2-テキスト文',  element: 'p',  styles: { 'font-size': '1em','margin': '10px 0 10px 25px' } },
+  { name: '段3-テキスト文',  element: 'p',  styles: { 'font-size': '1em','margin': '10px 0 10px 40px' } },
+  { name: '段4-テキスト文',  element: 'p',  styles: { 'font-size': '1em','margin': '10px 0 10px 55px' } },
+  // Inline Styles
+  { name: 'マーカー黄',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #ffff66 45%)' } },
+  { name: 'マーカーピンク',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #ffc9db 45%)'  } },
+  { name: 'マーカー水色',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #acffff 45%)'  } },
+  { name: 'マーカー緑',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #b8ff92 45%)'  } },
+  { name: 'マーカーベージュ',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #ffd791 45%)'  } },
+  { name: 'マーカーグレー',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 45%, #ddd 45%)'  } },
+  { name: 'ライン赤',  element: 'span',  styles: { 'background': 'linear-gradient(transparent 90%, #ff5a5a 90%)'  } },
+] );
