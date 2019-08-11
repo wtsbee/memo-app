dde61a8b3a41f7dc2e1f4136115a9fc49ed197b8
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-08-11 23:57:09 +0900

    フォルダ名・更新日時によるソート機能の追加

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,6 +1,10 @@
 class FoldersController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
-    @folders = Folder.all.includes(:memos).order("name ASC")
+    # @folders = Folder.all.includes(:memos).order("name ASC")
+    @folders = Folder.order(sort_column + ' ' + sort_direction)
+    # @folders = Folder.all.includes(:memos).order(params[:sort])
   end
 
   def create
@@ -18,5 +22,13 @@ class FoldersController < ApplicationController
   private
   def folder_params
     params.permit(:name).merge(user_id: current_user.id)
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
+
+  def sort_column
+      Folder.column_names.include?(params[:sort]) ? params[:sort] : "name"
   end
 end
