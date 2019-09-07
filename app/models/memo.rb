a0c9c3267019c00743e8358d9aff23eb934a24d0
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-09-07 18:12:20 +0900

    アソシエーションの設定

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,5 +1,6 @@
 class Memo < ApplicationRecord
   belongs_to :folder
+  belongs_to :user
   validates :name, presence: true
   mount_uploader :image, ImageUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :folders,      dependent: :destroy
+  has_many :memos
 end
