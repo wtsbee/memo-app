910f28e05f6d1d2606d3a7e18ca8d940781319c1
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-09-07 18:10:31 +0900

    カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_102754) do
+ActiveRecord::Schema.define(version: 2019_09_07_085641) do
 
   create_table "ckeditor_assets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "data_file_name", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_07_25_102754) do
     t.integer "folder_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
