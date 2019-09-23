859816292cac2f8ea74a6d9afcfa90c28c878fe4
wtsbee <beechan920@yahoo.co.jp>
2019-09-24 00:18:35 +0900

    [エラー対応]carrierwave.rbの編集

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module MemoApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
-    config.assets.initialize_on_precompile = false
   end
 end
+
+# config.assets.initialize_on_precompile = false
